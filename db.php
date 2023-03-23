87f8c9a4a72815f2f188951d0a14d1c82771773d
amurph24 <amurph@student.ubc.ca>
2023-03-23 16:09:10 -0700

    update db.php

--- db.php
+++ db.php
@@ -1,35 +1,35 @@
 <html>
     <body>
-<?php
+        <?php
 
-$servername = "localhost";
-$username = "10967677";
-$password = "10967677";
-$dbname = "db_10967677";
+        $servername = "localhost";
+        $username = "10967677";
+        $password = "10967677";
+        $dbname = "db_10967677";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    echo "connection failed";
-  die("Connection failed: " . $conn->connect_error);
-}
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            echo "connection failed";
+        die("Connection failed: " . $conn->connect_error);
+        }
 
-echo "<p>connection successful</p>";
+        echo "<p>connection successful</p>";
 
-$sql = "SELECT * FROM Category";
-$result = $conn->query($sql);
-echo "<p>";
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "title: " . $row["category_title"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-echo "</p>"
+        $sql = "SELECT * FROM Category";
+        $result = $conn->query($sql);
+        echo "<p>";
+        if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "title: " . $row["title"]. "<br>";
+        }
+        } else {
+        echo "0 results";
+        }
+        echo "</p>"
 
-?>
+        ?>
     </body>
 </html>
