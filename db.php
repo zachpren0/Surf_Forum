9e5f25c614ed92852721d43c2ee4c5698cfb22d0
amurph24 <amurph@student.ubc.ca>
2023-03-23 15:13:19 -0700

    add db.php

--- db.php
+++ db.php
@@ -1,3 +1,5 @@
+<html>
+    <body>
 <?php
 
 $servername = "localhost";
@@ -9,8 +11,12 @@ $dbname = "db_10967677";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
+    echo "connection failed";
   die("Connection failed: " . $conn->connect_error);
 }
 
+echo "connection successful";
 
 ?>
+    </body>
+</html>
