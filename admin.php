42035226c47cbac8fb8a8d7e8d26a5019d47c967
amurph24 <amurph@student.ubc.ca>
2023-04-05 17:30:25 -0700

    admin page changes

--- admin.php
+++ admin.php
@@ -71,8 +71,29 @@ include_once 'header.php';
                       <tr>
                         <th scope="row"><a href="account.php"> User_Name </a></th>
                         <td><a href="post.php"> Post_Name </a></td>
-                        
                       </tr>
+                      <?php
+                      $stmt = mysqli_stmt_init($conn);
+                      $sql = "SELECT * FROM USER";
+                      if (!mysqli_stmt_prepare($stmt, $sql)){
+                        header("location: ../admin.php?error=stmtFailed");
+                      }
+                      mysqli_stmt_execute($stmt);
+    
+                      $result = mysqli_stmt_get_result($stmt);
+                      if($result->num_rows > 0){
+                        while($row = $result->fetch_assoc()) {
+                          echo '<tr>';
+                          echo '<th scope="row">'.$row['username'].'</th>';
+                          echo '<td>'.$row['email'].'</td>';
+                          echo '</tr>';
+                        }
+                      } else {
+                        echo "No users found";
+                      }
+                        
+                        mysqli_stmt_close($stmt);
+                      ?>
                       <tr>
                         <th scope="row">DummyUser</th>
                         <td>Post_Name_2</td>
